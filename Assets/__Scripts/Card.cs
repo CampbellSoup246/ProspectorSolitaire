578edaaab8e895afa1c7eb90bfc0d6943a8414a2
CampbellSoup246 <campbellsoup246@gmail.com>
2018-11-03 16:01:23 -0400

    Setting up project and getting sprites prepared.

---
+++ Assets/__Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Class for each individual cardin the deck. Also contain CardDefinition class, holds info as where sprites positioned on each rank of card.
+
+public class Card : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
